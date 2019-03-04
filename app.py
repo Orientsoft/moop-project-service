d321f627ed0d453d395610ec2424b1ee677d1a54
nickery <xuyilin@orientsoft.cn>
2019-03-04 10:19:36 +0800

    修改url后缀

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config.from_object(config)
 
 def register_blueprint():
     from application.project import projects
-    app.register_blueprint(projects, url_prefix='/server/v1')
+    app.register_blueprint(projects, url_prefix='/service/v1')
 
 
 register_blueprint()
